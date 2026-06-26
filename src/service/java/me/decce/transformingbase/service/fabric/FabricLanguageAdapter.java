ff68c51190913149227c33c201d07a53c561f919
decce6 <decce6@proton.me>
2026-06-26 15:38:52 +0800

    Attempt to boot from `LanguageAdapter` on Fabric

---
+++ src/service/java/me/decce/transformingbase/service/fabric/FabricLanguageAdapter.java
@@ -0,0 +1,22 @@
+package me.decce.transformingbase.service.fabric;
+
+//? fabric {
+import me.decce.transformingbase.service.Bootstrapper;
+import net.fabricmc.loader.api.LanguageAdapter;
+import net.fabricmc.loader.api.ModContainer;
+
+// Note: LanguageAdapter seems to be the earliest available entrypoint on Fabric, which allows mods in PreLaunch to
+// benefit from asynchronous logging and be filtered
+// TODO: find a way to ensure we run *after* CrashAssistant
+@SuppressWarnings("unused")
+public class FabricLanguageAdapter implements LanguageAdapter {
+    public FabricLanguageAdapter() {
+        Bootstrapper.bootstrap();
+    }
+
+    @Override
+    public <T> T create(ModContainer mod, String value, Class<T> type) {
+        throw new RuntimeException();
+    }
+}
+//? }
