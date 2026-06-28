e7424d87e2005025c715e8d156fd71c955cba370
decce6 <decce6@proton.me>
2026-06-28 08:24:57 +0800

    Fix server-side mixin

--- src/mod-src/java/me/decce/asynclogger/mixins/ServerMainMixin.java
+++ src/mod-src/java/me/decce/asynclogger/mixins/ServerMainMixin.java
@@ -1,14 +1,14 @@
 package me.decce.asynclogger.mixins;
 
 import me.decce.transformingbase.core.LoggerConfigurator;
-import net.minecraft.client.main.Main;
+import net.minecraft.server.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Main.class)
-public class MainMixin {
+public class ServerMainMixin {
     //? neoforge && <1.21.9 {
     /*@Inject(method = "main", at = @At("HEAD"))
     private static void asynclogger$main(String[] strings, CallbackInfo ci) {
